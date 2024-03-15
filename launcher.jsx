ecc77225411ba303f44e5766d72865173805e7c4
xazzi <bret.combe@gmail.com>
2024-03-15 10:24:45 -0600

    servers adjusted

--- launcher.jsx
+++ launcher.jsx
@@ -7,14 +7,15 @@ if(new RegExp("Macintosh","g").exec($.os) == "Macintosh"){
     var platform = {
         name: "Macintosh",
         username: $.getenv('USER'),
-        directory: "",
+        support: "",
         local: paths.join("\\")
     }
 }else if(new RegExp("Windows","g").exec($.os) == "Windows"){
     var platform = {
         name: "Windows",
         username: $.getenv('USERNAME'),
-        directory: "//amz-impsw-data.us.digitalroominc.com/IMPSW_DATA/Support/",
+        support: "//amz-impsw-data.us.digitalroominc.com/IMPSW_DATA/Support/",
+        liveServices: "//amz-impsw-data.us.digitalroominc.com/IMPSW_DATA/.Live Services/",
         //directory: "//amz-impsw-data/IMPSW_DATA/Support/",
         local: paths.join("\\")
     }
