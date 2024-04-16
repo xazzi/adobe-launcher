4646f97de96c3938153bcc8bb89593a850949caa
xazzi <bret.combe@gmail.com>
2024-04-16 10:38:07 -0600

    push

--- processes/vinyl-lettering/vl-save.jsx
+++ processes/vinyl-lettering/vl-save.jsx
@@ -1,25 +1,34 @@
 try{
 
     var dir = {
+        xml: new Folder(platform.directory + "/.itemData"),
         ready: new Folder(platform.directory + "/Vinyl Lettering/Ready/"),
-        complete: new Folder(platform.directory + "/Vinyl Lettering/Complete/")
+        complete: new Folder(platform.directory + "/Vinyl Lettering/Complete_Dev/")
     }
 
-    var $doc = app.activeDocument
+    var doc = app.activeDocument
+
+    // Load in the data from the xml that Switch created.
+    //var dataFileXML = File(dir.xml + "/" + doc.name.split('.')[0].split('_')[4] + ".xml");
+    var dataFile = loadXmlFile(File(dir.xml + "/" + doc.name.split('.')[0].split('_')[4] + ".xml"));
+
+    var handoffData = {
+        contentFile: dataFile.product.contentFile.text()
+    }
 
     // Find the file for deletion later.
     var userFolders = getUserFolders(dir)
     for(var i in userFolders){
-        var file = new File(dir.ready + "/" + userFolders[i] + "/" + $doc.name);
+        var file = new File(dir.ready + "/" + userFolders[i] + "/" + doc.name);
         if(file.exists){
             break;
         }
     }
 
-    var allLayers = $doc.layers;
+    var allLayers = doc.layers;
     var lastIndex = allLayers.length-1;
 
-    var baseName = $doc.name.split('.')[0]
+    var baseName = doc.name.split('.')[0]
 
     var saveFiles = true;
     for(var j=0; j<allLayers.length; j++){
@@ -72,16 +81,16 @@ try{
                 var pdfOptions = new PDFSaveOptions();
                     pdfOptions.pDFPreset = "[Illustrator Default]";
 
-                var outfolder = makeOrGetFolder(dir.complete + "/" + facility + "/" + allLayers[0].name);
+                var outfolder = makeOrGetFolder(dir.complete + "/" + facility + "/" + allLayers[0].name + "/" + handoffData.contentFile.split('.pdf')[0]);
                 
                 var outfile = new File(outfolder + "/" + baseName + "_" + allLayers[0].name + ".pdf");
-                    $doc.saveAs(outfile, pdfOptions);
+                    doc.saveAs(outfile, pdfOptions);
             }
             lastIndex--;
             app.undo();
         }
 
-        $doc.close(SaveOptions.DONOTSAVECHANGES)
+        doc.close(SaveOptions.DONOTSAVECHANGES)
         file.remove()
     }
 
